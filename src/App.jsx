5e7fd1d763d37e23270c863fc8265452e7b28a81
kit-kat1320 <132790176+kit-kat1320@users.noreply.github.com>
2023-09-30 16:06:50 -0500

    emoji-box

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+// import ScoreKeeper from './ScoreKeeper'
+// import Counter from './Counter'
+import EmojiClicker from './EmojiClicker'
+
+
+function App() {
+  return(
+    <EmojiClicker/>
+  )
+}
+
+// function App() {
+//   return (
+//     <ScoreKeeper />
+//   );
+// }
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//     <div>
+//       <h1>Hello World</h1>
+//       <Counter />
+//     </div>
+//     </>
+//   );
+// }
+
+export default App
